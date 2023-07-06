3b2abd8672ae1cc7a30552e6f81b9453c11340ad
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-06 20:42:41 +0400

    Configurate tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="p-10">Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo aliquam
+        neque, dolore expedita officia temporibus facere delectus veniam vero
+        commodi quo at maiores sapiente. Culpa esse soluta eum. Quos, nesciunt?
+      </p>
+    </>
+  );
 }
 
 export default App;
