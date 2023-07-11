7f1bb56ee16ecf884368ca62b5c173271e586902
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-11 17:27:33 +0400

    Update ThemeSwitcher

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,9 +1,10 @@
-import { BsMoonStars } from "react-icons/bs";
+import { BsMoonStars, BsSunFill } from "react-icons/bs";
 
 const ThemeSwitcher = () => {
   return (
     <button className="fixed top-4 right-8 p-3 text-2xl rounded-full bg-grey">
       <BsMoonStars className="text-grey-dark" />
+      {/* <BsSunFill className="text-white" /> */}
     </button>
   );
 };
