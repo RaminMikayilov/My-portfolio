cdfdb889299cd1ae48bc20d7ec9c6be7121946e9
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-13 11:29:30 +0400

    Update experience section

--- src/components/About/Experience.jsx
+++ src/components/About/Experience.jsx
@@ -5,33 +5,35 @@ import ExperienceCard from "./other/ExperienceCard";
 const Experience = () => {
   return (
     <div className="text-grey-dark">
-      <h1 className="text-xl font-semibold uppercase mb-10">
+      <h2 className="text-xl font-semibold uppercase mb-10 sm:mb-12 sm:text-2xl md:text-[26px] sm:text-center">
         Experience & education
-      </h1>
+      </h2>
 
-      {/* experince */}
-      <div className="space-y-12 uppercase">
-        {experience.map((exp, index) => (
-          <ExperienceCard
-            key={index}
-            time={exp.time}
-            text1={exp.title}
-            text3={exp.company}
-          />
-        ))}
-      </div>
+      <div className="grid grid-col-1 lg:grid-cols-2">
+        {/* experince */}
+        <div className="space-y-12 uppercase">
+          {experience.map((exp, index) => (
+            <ExperienceCard
+              key={index}
+              time={exp.time}
+              text1={exp.title}
+              text3={exp.company}
+            />
+          ))}
+        </div>
 
-      {/* education */}
-      <div className="space-y-12 uppercase mt-12">
-        {education.map((edu, index) => (
-          <ExperienceCard
-            key={index}
-            time={edu.time}
-            text1={edu.degree}
-            text2={edu.school}
-            text3={edu.major}
-          />
-        ))}
+        {/* education */}
+        <div className="space-y-12 uppercase mt-16 lg:mt-0">
+          {education.map((edu, index) => (
+            <ExperienceCard
+              key={index}
+              time={edu.time}
+              text1={edu.degree}
+              text2={edu.school}
+              text3={edu.major}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
