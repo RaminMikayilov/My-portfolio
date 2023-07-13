2c61343236e04192f74902d70bcdc2ceee4990a8
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-13 11:15:24 +0400

    Add ExperienceCard

--- src/components/About/Experience.jsx
+++ src/components/About/Experience.jsx
@@ -1,6 +1,6 @@
 import education from "../../data/education";
 import experience from "../../data/experience";
-import { FaBriefcase } from "react-icons/fa";
+import ExperienceCard from "./other/ExperienceCard";
 
 const Experience = () => {
   return (
@@ -12,38 +12,25 @@ const Experience = () => {
       {/* experince */}
       <div className="space-y-12 uppercase">
         {experience.map((exp, index) => (
-          <div key={index} className="flex items-start">
-            <div className="p-3 text-xl rounded-full bg-orange text-white relative after:content-[''] after:absolute after:top-[100%] after:left-1/2 after:bg-[#ddd] after:w-[2px] after:h-[65px]">
-              {<FaBriefcase />}
-            </div>
-            <div className="px-4 space-y-4">
-              <span className="bg-grey text-xs font-medium rounded-full px-2 py-1">
-                {exp.time}
-              </span>
-              <h2 className="font-semibold">{exp.title}</h2>
-              <p>{exp.company}</p>
-            </div>
-          </div>
+          <ExperienceCard
+            key={index}
+            time={exp.time}
+            text1={exp.title}
+            text3={exp.company}
+          />
         ))}
       </div>
 
       {/* education */}
       <div className="space-y-12 uppercase mt-12">
         {education.map((edu, index) => (
-          <div key={index} className="flex items-start">
-            <div className="p-3 text-xl rounded-full bg-orange text-white relative after:content-[''] after:absolute after:top-[100%] after:left-1/2 after:bg-[#ddd] after:w-[2px] after:h-[65px]">
-              {<FaBriefcase />}
-            </div>
-            <div className="px-4 space-y-4">
-              <span className="bg-grey text-xs font-medium rounded-full px-2 py-1">
-                {edu.time}
-              </span>
-              <h2 className="font-semibold">
-                {edu.degree} - <span className="font-normal">{edu.school}</span>
-              </h2>
-              <p className="text-sm">{edu.major}</p>
-            </div>
-          </div>
+          <ExperienceCard
+            key={index}
+            time={edu.time}
+            text1={edu.degree}
+            text2={edu.school}
+            text3={edu.major}
+          />
         ))}
       </div>
     </div>
---
+++ src/components/About/other/ExperienceCard.jsx
@@ -0,0 +1,22 @@
+import { FaBriefcase } from "react-icons/fa";
+
+const ExperienceCard = ({ time, text1, text2, text3 }) => {
+  return (
+    <div className="flex items-start">
+      <div className="p-3 text-xl rounded-full bg-orange text-white relative after:content-[''] after:absolute after:top-[100%] after:left-1/2 after:bg-[#ddd] after:w-[2px] after:h-[65px]">
+        {<FaBriefcase />}
+      </div>
+      <div className="px-4 space-y-4">
+        <span className="bg-grey text-xs font-medium rounded-full px-2 py-1">
+          {time}
+        </span>
+        <h2 className="font-semibold">
+          {text1} {text2 && <span className="font-normal">- {text2}</span>}
+        </h2>
+        <p>{text3}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceCard;
