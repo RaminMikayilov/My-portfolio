38687bcdb449120bdc855ca68a1981165e344d03
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-14 00:08:14 +0400

    Add portfolio page

---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+import youtubeclone from "../assets/images/projects/youtubeclone.png";
+import movieapp from "../assets/images/projects/movieapp.png";
+import dictionary from "../assets/images/projects/dictionary.png";
+import todo from "../assets/images/projects/todo.png";
+import colorgenerator from "../assets/images/projects/colorgenerator.png";
+
+export const projects = [
+  {
+    name: "Youtube Clone",
+    image: youtubeclone,
+    github: "https://github.com/RaminMikayilov/YouTube-clone",
+    live: "https://rm-youtubeclone.netlify.app/",
+  },
+  {
+    name: "Movie App",
+    image: movieapp,
+    github: "https://github.com/RaminMikayilov/Movie-app",
+    live: "https://rm-movieapp.netlify.app/",
+  },
+  {
+    name: "Dictionary App",
+    image: dictionary,
+    github: "https://github.com/RaminMikayilov/Dictionary-web-app",
+    live: "https://rm-dictionary-web-app.netlify.app/",
+  },
+  {
+    name: "Todo List",
+    image: todo,
+    github: "https://github.com/RaminMikayilov/Redux-Todo-List",
+    live: "https://rm-rtk-todo-list-app.netlify.app/",
+  },
+  {
+    name: "Color Generator",
+    image: colorgenerator,
+    github: "https://github.com/RaminMikayilov/Color-generator",
+    live: "https://rm-colorgenerator.netlify.app/",
+  },
+];
