859a420d857c87ffbb01e2a89fc3429eba8a1bb3
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-13 15:48:44 +0400

    Add contact section

---
+++ src/data/contact.js
@@ -0,0 +1,36 @@
+import {
+  FaMapMarkedAlt,
+  FaEnvelopeOpen,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { FaPhoneVolume } from "react-icons/fa6";
+
+export const contact = [
+  {
+    Icon: FaMapMarkedAlt,
+    text1: "address point",
+    text2: "Baku City, Azerbaijan",
+  },
+  {
+    Icon: FaEnvelopeOpen,
+    text1: "mail me",
+    text2: "raminmikayilovdev@gmail.com",
+  },
+  {
+    Icon: FaPhoneVolume,
+    text1: "call me",
+    text2: "+994 55 836 26 88",
+  },
+];
+
+export const social = [
+  {
+    Icon: FaGithub,
+    link: "https://www.github.com/raminmikayilov/",
+  },
+  {
+    Icon: FaLinkedin,
+    link: "https://www.linkedin.com/in/ramin-mikayilov/",
+  },
+];
