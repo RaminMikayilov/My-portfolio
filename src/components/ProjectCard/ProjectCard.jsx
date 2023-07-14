bebf6bd3da5d2d592d1528453e19b8ef902b9431
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-15 01:36:01 +0400

    Update design of ProjectCard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -6,8 +6,8 @@ const ProjectCard = ({ name, image, github, live }) => {
   return (
     <div className="project__card p-5 mb-10 w-[270px] xs:w-[300px] sm:w-[260px] md:w-[290px] xl:w-[300px] 2xl:w-[320px] border-2 border-orange rounded-xl hover:shadow-lg duration-300 mx-auto">
       <img src={image} alt={name} className="rounded-xl" />
-      <div className="font-medium flex justify-between items-center mt-5">
-        <h1>{name}</h1>
+      <div className="flex justify-between items-center mt-5">
+        <h1 className="font-medium ">{name}</h1>
         <span className="flex gap-x-2 text-2xl">
           <a
             href={github}
